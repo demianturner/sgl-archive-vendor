ab689cb25c2fb9d8276488954b2daf8e227f5aa3
Demian Turner <demian@seagullsystems.com>
2010-02-22 18:07:22 +0000

    updating copyright year -3

--- pear/Image/Transform/Driver/GD_SGL.php
+++ pear/Image/Transform/Driver/GD_SGL.php
@@ -1,7 +1,7 @@
 <?php
 /* Reminder: always indent with 4 spaces (no tabs). */
 // +---------------------------------------------------------------------------+
-// | Copyright (c) 2008, Demian Turner                                         |
+// | Copyright (c) 2010, Demian Turner                                         |
 // | All rights reserved.                                                      |
 // |                                                                           |
 // | Redistribution and use in source and binary forms, with or without        |
